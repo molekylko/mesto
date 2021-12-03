003b174344ef8cb2a7b655a6259854c2c7ffa776
molekylko <molekylko@gmail.com>
2021-12-03 14:53:47 +0300

    feat: форма заработала, можно менять имя

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+/* Модальное окно */
+
 const editBtn = document.querySelector(".profile__edit-btn");
 const editForm = document.querySelector(".edit-form");
 const closeBtn = document.querySelector(".edit-form__close-btn");
@@ -16,7 +18,24 @@ function closeEditForm() {
 }
 
 
-
 editBtn.addEventListener('click', openEditForm);
 closeBtn.addEventListener('click', closeEditForm);
 closeOverlay.addEventListener('click', closeEditForm);
+
+
+/* Изменение инфы в профиле */
+let username = document.querySelector('.profile__title');
+let profession = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector(".edit-form__form");
+let usernameInput = document.querySelector('.input__text_type_username');
+let professionInput = document.querySelector('.input__text_type_profession');
+let saveInfoBtn = document.querySelector('.edit-form__submit-btn');
+
+
+formElement.onsubmit = function(evt)  {
+    evt.preventDefault();
+    username.textContent = usernameInput.value;
+    profession.textContent = professionInput.value;
+};
+
+saveInfoBtn.addEventListener('click', closeEditForm);
