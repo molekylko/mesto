e4be0e8261836be9d467cdae06f3416078e9fe62
molekylko <molekylko@gmail.com>
2021-12-06 02:14:22 +0300

    исправления после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,25 @@
 /* Модальное окно */
 
 const editBtn = document.querySelector(".profile__edit-btn");
-const editForm = document.querySelector(".edit-form");
-const closeBtn = document.querySelector(".edit-form__close-btn");
-const closeOverlay = document.querySelector(".edit-form__overlay");
-
+const editForm = document.querySelector(".popup");
+const closeBtn = document.querySelector(".popup__close-btn");
+const closeOverlay = document.querySelector(".popup__overlay");
+let username = document.querySelector('.profile__title');
+let profession = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector(".popup__form");
+let usernameInput = document.querySelector('.popup__text_type_username');
+let professionInput = document.querySelector('.popup__text_type_profession');
+let saveInfoBtn = document.querySelector('.popup__submit-btn');
 
 function openEditForm() {
-    editForm.classList.add("edit-form_active")
-    document.body.classList.add("body_overflow_hidden")
+    editForm.classList.add("popup_active")
+    usernameInput.value = username.textContent;
+    professionInput.value = profession.textContent;
 }
 
 
 function closeEditForm() {
-    editForm.classList.remove("edit-form_active")
-    document.body.classList.remove("body_overflow_hidden")
+    editForm.classList.remove("popup_active")
 }
 
 
@@ -24,18 +29,9 @@ closeOverlay.addEventListener('click', closeEditForm);
 
 
 /* Изменение инфы в профиле */
-let username = document.querySelector('.profile__title');
-let profession = document.querySelector('.profile__subtitle');
-let formElement = document.querySelector(".edit-form__form");
-let usernameInput = document.querySelector('.input__text_type_username');
-let professionInput = document.querySelector('.input__text_type_profession');
-let saveInfoBtn = document.querySelector('.edit-form__submit-btn');
-
-
 formElement.onsubmit = function(evt)  {
     evt.preventDefault();
     username.textContent = usernameInput.value;
     profession.textContent = professionInput.value;
+    closeEditForm();
 };
-
-saveInfoBtn.addEventListener('click', closeEditForm);
